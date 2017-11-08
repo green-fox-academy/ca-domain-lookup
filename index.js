01eda76650fc89e4211ae42cf6c4a44805eb1bb4
Szász Tibor <tszasz@gmail.com>
2017-11-08 14:12:52 +0100

    feat(backend): endpoints created

--- index.js
+++ index.js
@@ -5,12 +5,26 @@
  * 3)  - find method to test domain existence
  * 4) send it back to client
  */
-
+const port = 3000;
 const { exec } = require('child_process');
-exec('nslookup 444.hu', (err, stdout, stderr) => {
-    console.log(errorCheck(stderr));
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/check', (req, res) => {
+    exec('nslookup 444.hu', (err, stdout, stderr) => {
+        console.log(errorCheck(stderr));
+    });
+    res.sendFile(__dirname + '/index.html');
 });
 
 function errorCheck( errString ) {
     return ( errString.indexOf('Non-existent domain') == -1 );
 }
+
+app.listen(port, () => {
+    console.log('App is listening on http://localhost:' + port );
+});
